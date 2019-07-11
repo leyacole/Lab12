db80b29e9ec1215356b50501f6cff1b4a45bdce6
leyacole <50380624+leyacole@users.noreply.github.com>
2019-07-11 09:13:01 -0400

    Update CarApp2.java

--- src/CarApp2.java
+++ src/CarApp2.java
@@ -1,10 +1,21 @@
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CarApp2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner scnr = new Scanner(System.in);
 		
+		ArrayList<Car> car = new ArrayList<>();
+		
+		car.add(new Car("Jeep", "Grand Cherokee", 2016, 50000.00));
+		car.add(new Car("Jeep", "Cherokee", 2018, 30000.00));
+		car.add(new Car("Cadillac", "CTS", 2016, 60000.00));
+		car.add(new UsedCar("Chevorlet", "Camero", 2019, 55000.00, 100000.00));
+		car.add(new UsedCar("Ford", "F-150", 1999, 50000.00, 80000.00));
 		
 	}
 
